1ec573e94ab16375748aa892b461a0704d3650ee
mukosei221111 <99156806+mukosei221111@users.noreply.github.com>
2022-09-29 10:28:21 +0800

    Update Get-DmmUrl.ps1

--- src/Javinizer/Public/Get-DmmUrl.ps1
+++ src/Javinizer/Public/Get-DmmUrl.ps1
@@ -15,48 +15,48 @@ function Get-DmmUrl {
     )
 
     process {
-        $r18Results = Get-R18Url -Id $Id -Strict:$Strict -AllResults -WarningAction SilentlyContinue
-        $resultObject = foreach ($entry in $r18Results) {
-            $cid = (($entry.En -split 'id=')[1] -split '\/')[0]
-            [PSCustomObject]@{
-                Id        = $Id
-                ContentId = $cid
-                Title     = $entry.Title
-                Url       = "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=$cid"
-            }
-        }
+        # $r18Results = Get-R18Url -Id $Id -Strict:$Strict -AllResults -WarningAction SilentlyContinue
+        # $resultObject = foreach ($entry in $r18Results) {
+            # $cid = (($entry.En -split 'id=')[1] -split '\/')[0]
+            # [PSCustomObject]@{
+                # Id        = $Id
+                # ContentId = $cid
+                # Title     = $entry.Title
+                # Url       = "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=$cid"
+            # }
+        # }
+		$ContentId = $Id
+        # if ($Id -in $resultObject.Id) {
+            # $matchedResult = $resultObject | Where-Object { $Id -eq $_.Id }
 
-        if ($Id -in $resultObject.Id) {
-            $matchedResult = $resultObject | Where-Object { $Id -eq $_.Id }
+            # if ($matchedResult.Count -gt 1 -and !($AllResults)) {
+                # $matchedResult = $matchedResult[0]
+            # }
 
-            if ($matchedResult.Count -gt 1 -and !($AllResults)) {
-                $matchedResult = $matchedResult[0]
-            }
+            # $urlObject = foreach ($entry in $matchedResult) {
+                # [PSCustomObject]@{
+                    # En    = "https://www.dmm.co.jp/en/mono/dvd/-/detail/=/cid=$($entry.ContentId)"
+                    # Ja    = "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=$($entry.ContentId)"
+                    # Id    = $entry.Id
+                    # Title = $entry.Title
+                # }
+            # }
+            # Write-Output $urlObject
 
-            $urlObject = foreach ($entry in $matchedResult) {
-                [PSCustomObject]@{
-                    En    = "https://www.dmm.co.jp/en/mono/dvd/-/detail/=/cid=$($entry.ContentId)"
-                    Ja    = "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=$($entry.ContentId)"
-                    Id    = $entry.Id
-                    Title = $entry.Title
-                }
-            }
-            Write-Output $urlObject
-
-        } else {
+        # } else {
             # The digital/videoa URL is not being caught by the html for movie IDs matching '0001 - 0009'
             # Convert the movie Id (ID-###) to content Id (ID00###) to match dmm naming standards
-            if (!($Strict)) {
-                if ($Id -match '([a-zA-Z|tT28|rR18]+-\d+z{0,1}Z{0,1}e{0,1}E{0,1})') {
-                    $splitId = $Id -split '-'
-                    if (($splitId[1])[-1] -match '\D') {
-                        $appendChar = ($splitId[1])[-1]
-                        $splitId[1] = $splitId[1] -replace '\D', ''
-                    }
-                    $contentId = ($splitId[0] + $splitId[1].PadLeft(5, '0') + $appendChar).Trim()
-                }
-                $cleanId = (($splitId -join "") + $appendChar).Trim()
-            }
+            # if (!($Strict)) {
+                # if ($Id -match '([a-zA-Z|tT28|rR18]+-\d+z{0,1}Z{0,1}e{0,1}E{0,1})') {
+                    # $splitId = $Id -split '-'
+                    # if (($splitId[1])[-1] -match '\D') {
+                        # $appendChar = ($splitId[1])[-1]
+                        # $splitId[1] = $splitId[1] -replace '\D', ''
+                    # }
+                    # $contentId = ($splitId[0] + $splitId[1].PadLeft(5, '0') + $appendChar).Trim()
+                # }
+                # $cleanId = (($splitId -join "") + $appendChar).Trim()
+            # }
 
             $searchUrl = "https://www.dmm.co.jp/search/=/searchstr=$contentId/"
 
@@ -67,26 +67,40 @@ function Get-DmmUrl {
                 Write-JVLog -Write:$script:JVLogWrite -LogPath $script:JVLogPath -WriteLevel $script:JVLogWriteLevel -Level Error -Message "[$Id] [$($MyInvocation.MyCommand.Name)] Error occured on [GET] on URL [$searchUrl]: $PSItem" -Action 'Continue'
             }
 
-            $searchResults = $webRequest.links.href | Where-Object { $_ -match '\/mono\/dvd' -or $_ -match '\/digital\/videoa' }
+            $searchResults = $webRequest.links.href | Where-Object { $_ -match '\/mono\/dvd' -or $_ -match '\/digital\/video' }
 
             if ($searchResults) {
                 $matchedResults = @()
 
                 # Prioritize digital video before falling back to DVD matches
-                if ($searchResults -match "\/digital\/videoa") {
-                    $searchResults | Where-Object { $_ -match "\/digital\/videoa" -and $_ -match $contentId } | ForEach-Object {
-                        $cid = ($_ | Select-String -Pattern 'cid=(.*)\/').Matches.Groups[1].Value
-
+                if ($searchResults -match "\/digital\/videoc") {
+                    $searchResults | Where-Object { $_ -match "\/digital\/videoc" -and $_ -match $contentId } | ForEach-Object {
+                        # $cid = ($_ | Select-String -Pattern 'cid=(.*)\/').Matches.Groups[1].Value
+                        $cid = $contentId
                         # Remove the prepended numbers in the contentId to more accurately match it to generic cid value
-                        $cleanCid = $cid -replace '^\w*?(?=[a-z]+\d+)', ''
+                        # $cleanCid = $cid -replace '^\w*?(?=[a-z]+\d+)', ''
 
                         # Digital videos will match to contentId (ID00123)
-                        if ($cleanCid -eq $contentId) {
-                            $matchedResults += $_
-                        }
+                        # if ($cleanCid -eq $contentId) {
+                        $matchedResults += $_
+                        # }
                     }
                 }
 
+                if ($searchResults -match "\/digital\/videoa") {
+                    $searchResults | Where-Object { $_ -match "\/digital\/videoa" -and $_ -match $contentId } | ForEach-Object {
+                        # $cid = ($_ | Select-String -Pattern 'cid=(.*)\/').Matches.Groups[1].Value
+                        $cid = $contentId
+                        # Remove the prepended numbers in the contentId to more accurately match it to generic cid value
+                        # $cleanCid = $cid -replace '^\w*?(?=[a-z]+\d+)', ''
+
+                        # Digital videos will match to contentId (ID00123)
+                        # if ($cleanCid -eq $contentId) {
+                        $matchedResults += $_
+                        # }
+                    }
+                }
+				
                 if ($searchResults -match "\/mono\/dvd") {
                     $searchResults | Where-Object { $_ -match "\/mono\/dvd" -and $_ -match $cleanId } | ForEach-Object {
                         $cid = ($_ | Select-String -Pattern 'cid=(.*)\/').Matches.Groups[1].Value
@@ -112,6 +126,6 @@ function Get-DmmUrl {
                 Write-JVLog -Write:$script:JVLogWrite -LogPath $script:JVLogPath -WriteLevel $script:JVLogWriteLevel -Level Warning -Message "[$Id] [$($MyInvocation.MyCommand.Name)] not matched on DMM"
                 return
             }
-        }
+        # }
     }
 }
